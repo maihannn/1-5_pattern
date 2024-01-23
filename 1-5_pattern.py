0673a4c15219e5020999d26fc45cb6ba56a4d5a7
maihannn <maicapalillo15@gmail.com>
2024-01-23 22:11:01 +0800

    done, it worked

---
+++ 1-5_pattern.py
@@ -0,0 +1,11 @@
+# Name: Jamaica C. Palillo
+# Section: BSCPE 1-5
+
+# Exercise 8: = Print the following pattern
+
+
+row = 5
+for count in range (row+1): 
+    for i in range (count):
+        print (count, end=" ") 
+    print("")
